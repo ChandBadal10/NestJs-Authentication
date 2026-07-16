864cf058b193e8e97d37834581e3a69df58698e6
Badal Chand <badalchand636@gmail.com>
2026-07-16 17:17:18 +0545

    Completed the Update profile name, email

--- src/auth/strategies/jwt.strategy.ts
+++ src/auth/strategies/jwt.strategy.ts
@@ -37,12 +37,7 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
     if(!user) {
       throw new UnauthorizedException("User not found");
     }
-    return {
-      userId: user._id,
-      name: user.name,
-      email: user.email,
-      role: user.role,
-      isAccountVerified: user.isAccountVerified,
-    }
+    return user;
+
   }
   }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -2,6 +2,10 @@ import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { User, UserDocument } from './schemas/user.schema';
+import { UpdateProfileDto } from 'src/auth/dto/update-profile.dto';
+
+
+
 
 @Injectable()
 export class UsersService {
@@ -10,6 +14,8 @@ export class UsersService {
     private readonly userModel: Model<UserDocument>,
   ) {}
 
+
+  // find all users
   async findAllUsers() {
     const users = await this.userModel
       .find()
@@ -21,4 +27,70 @@ export class UsersService {
       users,
     };
   }
+
+
+  // get my profile
+
+  async getMyProfile(userId: string) {
+    const user = await this.userModel.findById(userId).select("-password");
+
+    if(!user) {
+      return {
+        success: false,
+        message: "User not found"
+      };
+    }
+    return {
+      success: true,
+      user,
+    }
+  }
+
+
+  // update profile
+  async updateProfile(userId: string, updateProfileDto: UpdateProfileDto) {
+    const {name, email} = updateProfileDto;
+
+    const user = await this.userModel.findById(userId);
+
+    if(!user) {
+      return {
+        success: false,
+        message: "User not found"
+      }
+    }
+
+    // check if the email is already used by another user
+
+    if(email && email !== user.email) {
+      const exisitingUser = await this.userModel.findOne({email});
+
+      if(exisitingUser) {
+        return {
+          success: false,
+          message: "Email Already exists"
+        };
+      }
+
+      user.email = email;
+
+    }
+
+    if(name) {
+      user.name = name;
+    }
+
+    await user.save();
+
+
+    const updateUser = await this.userModel.findById(userId).select("-password");
+
+    return {
+      success: true,
+      message: "Profile update successfully",
+      user: updateUser
+    }
+
+  }
+
 }
