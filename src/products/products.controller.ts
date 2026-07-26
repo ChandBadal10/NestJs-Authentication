a9e1bae7419f0daaa4d7694a5265138f12afc9ce
Badal Chand <badalchand636@gmail.com>
2026-07-26 10:34:56 +0545

    Successfully completed  the create Product API

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,9 +1,32 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post, Req, UseGuards } from '@nestjs/common';
 import { ProductsService } from './products.service';
+import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
+import { RolesGuard } from 'src/auth/guards/roles.guard';
+import { Roles } from 'src/auth/decorators/roles.decorator';
+import { Role } from 'src/auth/enums/role.enum';
+import { CreateProductDto } from './dto/create-product.dto';
+
+
+
+
+
 
 @Controller('products')
 export class ProductsController {
     constructor(
         private readonly productsService: ProductsService
     ) {}
+
+    @Post()
+    @UseGuards(JwtAuthGuard, RolesGuard)
+    @Roles(Role.ADMIN)
+    async createProduct(
+        @Body() createProductDto: CreateProductDto,
+        @Req() req: Request & {user: any},
+    ) {
+        return this.productsService.createProduct(
+            createProductDto,
+            req.user.id,
+        )
+    }
 }
--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,14 +1,104 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable, NotFoundException } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Product, ProductDocument } from './schemas/product.schema';
-import { Model } from 'mongoose';
+import { Model, Types } from 'mongoose';
+import { Category, CategoryDocument } from 'src/categories/schemas/category.schema';
+import { Brand, BrandDocument } from 'src/brands/schemas/brand.schema';
+import { CreateProductDto } from './dto/create-product.dto';
+import slugify from 'slugify';
+
+
+
 
 @Injectable()
 export class ProductsService {
     constructor(
         @InjectModel(Product.name)
-        private readonly productModel: Model<ProductDocument>
+        private readonly productModel: Model<ProductDocument>,
+
+        @InjectModel(Category.name)
+        private readonly categoryModel: Model<CategoryDocument>,
+
+        @InjectModel(Brand.name)
+        private readonly brandModel: Model<BrandDocument>
     ) {}
 
+    private async findCategory(id: string) {
+        return this.categoryModel.findById(id);
+    }
 
+    private async findBrand(id: string) {
+        return this.brandModel.findById(id);
+    }
+
+    private async findSku(sku: string) {
+        return this.productModel.findOne({ sku })
+    }
+
+
+    async createProduct(createProductDto: CreateProductDto, userId: Types.ObjectId) {
+        const  { name, description, price, discountPrice, stock, sku, category, brand, images, isFeatured, isPublished } = createProductDto;
+
+        //Check Category
+        const categoryExists = await this.findCategory(category);
+
+        if(!categoryExists) {
+            throw new NotFoundException("Category not found");
+        }
+
+        //check brand
+        const brandExists = await this.findBrand(brand);
+
+        if(!brandExists) {
+            throw new NotFoundException("Brand not found");
+        }
+
+        // Check SKU
+
+        const skuExists = await this.findSku(sku);
+
+        if(skuExists) {
+            throw new BadRequestException("SKU already exists");
+        }
+
+        // Validate Discount
+
+        if(discountPrice && discountPrice > price) {
+            throw new BadRequestException("Discount price cannot be greater than price");
+        }
+
+        // Generate slug
+        const slug = slugify(name, {
+            lower: true,
+            strict: true,
+            trim: true
+        })
+
+
+        // Create Product
+
+        const product = await this.productModel.create({
+            name,
+            slug,
+            description,
+            price,
+            discountPrice,
+            stock,
+            sku,
+            category,
+            brand,
+            images: [],
+            isFeatured,
+            isPublished,
+            createdBy: userId,
+        });
+
+        return {
+            success: true,
+            message: "Product created successfully",
+            data: product
+        }
+
+
+    }
 }
